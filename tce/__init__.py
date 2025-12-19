57b0da80dc29bbaba8f3906b7075f31c66442178
Jacob Jeffries <88559006+jwjeffr@users.noreply.github.com>
2025-12-19 09:12:46 -0500

    Bump version to 0.10.2

--- tce/__init__.py
+++ tce/__init__.py
@@ -513,7 +513,7 @@ and [leave-one-out cross validation](https://www.statology.org/leave-one-out-cro
 
 """
 
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 __authors__ = ["Jacob Jeffries"]
 
 __url__ = "https://github.com/MUEXLY/tce-lib"
